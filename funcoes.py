144e4e1626679990bd7c6f4f2e5540df9d97f6eb
unesrafael <rafaelau@al.insper.edu.br>
2024-10-27 18:57:25 -0300

    Q8

--- funcoes.py
+++ funcoes.py
@@ -76,4 +76,15 @@ def posicao_valida(dic_navios, linha, coluna, orientacao, tamanho):
             for posicao in posicoes:
                 if posicao in nova_posicao:
                     return False
-    return True     
+    return True
+
+def monta_tabuleiros(tabuleiro_jogador, tabuleiro_oponente):
+    texto = ''
+    texto += '   0  1  2  3  4  5  6  7  8  9         0  1  2  3  4  5  6  7  8  9\n'
+    texto += '_______________________________      _______________________________\n'
+
+    for linha in range(len(tabuleiro_jogador)):
+        jogador_info = '  '.join([str(item) for item in tabuleiro_jogador[linha]])
+        oponente_info = '  '.join([info if str(info) in 'X-' else '0' for info in tabuleiro_oponente[linha]])
+        texto += f'{linha}| {jogador_info}|     {linha}| {oponente_info}|\n'
+    return texto     
--- programa.py
+++ programa.py
@@ -31,7 +31,58 @@ for navio in frota_inicial:
                 break
             else:
                 print('Esta posição não está válida!')
-print(frota)
+
+frota_oponente = {
+    'porta-aviões': [
+        [[9, 1], [9, 2], [9, 3], [9, 4]]
+    ],
+    'navio-tanque': [
+        [[6, 0], [6, 1], [6, 2]],
+        [[4, 3], [5, 3], [6, 3]]
+    ],
+    'contratorpedeiro': [
+        [[1, 6], [1, 7]],
+        [[0, 5], [1, 5]],
+        [[3, 6], [3, 7]]
+    ],
+    'submarino': [
+        [[2, 7]],
+        [[0, 6]],
+        [[9, 7]],
+        [[7, 6]]
+    ]
+}
+tabuleiro_oponente = funcoes.posiciona_frota(frota_oponente)
+tabuleiro_jogador = funcoes.posiciona_frota(frota)
+jogando = True
+já_atacados = []
+        
+while jogando == True:
+    tabuleiros = funcoes.monta_tabuleiros(tabuleiro_jogador, tabuleiro_oponente)
+    print(tabuleiros)
+    i = 0
+    while i == 0:
+        linha = int(input('Digite a linha que deseja atacar: '))
+        while linha < 0 or linha > 9:
+            print('Linha inválida!')
+            linha = int(input('Digite a linha que deseja atacar: '))      
+        coluna = int(input('Digite a coluna que deseja atacar: '))
+        while coluna < 0 or coluna > 9:    
+            print ('Coluna inválida!')
+            coluna = int(input('Digite a coluna que deseja atacar: '))
+        if [linha, coluna] not in já_atacados:
+            já_atacados.append([linha,coluna])
+            i = 1
+        else:
+            print(f'A posição linha {linha} e coluna {coluna} já foi informada anteriormente!')
+          
+    tabuleiro_oponente = funcoes.faz_jogada(tabuleiro_oponente, linha, coluna)
+    total_afundados = funcoes.afundados(frota_oponente, tabuleiro_oponente)
+
+    if total_afundados == 10:
+        print('Parabéns! Você derrubou todos os navios do seu oponente!')
+        jogando = False
+        
 
 
 
